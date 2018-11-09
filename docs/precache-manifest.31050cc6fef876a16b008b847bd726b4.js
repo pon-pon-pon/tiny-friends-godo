1a81548dd3c586220b1c49f995baa9b582f561eb
pon-pon-pon <shoponimage@gmail.com>
2018-11-10 00:12:07 +0900

    v0.1

---
+++ docs/precache-manifest.31050cc6fef876a16b008b847bd726b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bb0bc8ccf31c61644cd",
+    "url": "/static/css/main.b8749e56.chunk.css"
+  },
+  {
+    "revision": "3bb0bc8ccf31c61644cd",
+    "url": "/static/js/main.3bb0bc8c.chunk.js"
+  },
+  {
+    "revision": "168df64267bd2a15457d",
+    "url": "/static/css/1.05fd42ab.chunk.css"
+  },
+  {
+    "revision": "168df64267bd2a15457d",
+    "url": "/static/js/1.168df642.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe4275137b895bb551009fc16c66b807",
+    "url": "/static/media/logo.fe427513.png"
+  },
+  {
+    "revision": "145740f9633de7a4c3b1011c21275dfc",
+    "url": "/static/media/jumbotron.145740f9.png"
+  },
+  {
+    "revision": "558f74c124f58e1f69c6bfd006c7f6b1",
+    "url": "/static/media/text.558f74c1.png"
+  },
+  {
+    "revision": "3ab3e26026fde4c5f196db098b6d247e",
+    "url": "/static/media/shopon.3ab3e260.jpg"
+  },
+  {
+    "revision": "8a967064b7f19c85a5c8b54f7056ee85",
+    "url": "/static/media/lolimoya.8a967064.jpg"
+  },
+  {
+    "revision": "fe1720b9decb6a9f5dea55aa07bb04f6",
+    "url": "/index.html"
+  }
+];
