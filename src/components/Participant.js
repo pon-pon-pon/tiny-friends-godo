1e1df4f068a2a5802f01d64b612a7e7533426649
pon-pon-pon <shoponimage@gmail.com>
2018-11-10 00:59:21 +0900

    font-weightを調整

--- src/components/Participant.js
+++ src/components/Participant.js
@@ -7,7 +7,7 @@ import ICON_T from '../assets/icon_t.png';
 const Name = styled.p`
     letter-spacing:     .064em;
     font-size:          1.5rem;
-    font-weight:        100;
+    font-weight:        200;
     transition:         transform .3s ease-out;
     transform:          translateY(-1rem);
     position:           absolute;
