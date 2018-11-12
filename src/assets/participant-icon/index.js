1c5b9d45b470196792c996db0879a09cbc5711bb
pon-pon-pon <shoponimage@gmail.com>
2018-11-12 19:08:24 +0900

    ダミーアイコンを定義

--- src/assets/participant-icon/index.js
+++ src/assets/participant-icon/index.js
@@ -1,9 +1,11 @@
+import ICON_DUMMY from './dummy.jpg';
 import ICON_AMANO from './amano.jpg';
 import ICON_UTOI from './utoi.jpg';
 import ICON_KAGE from './kage.jpg';
 import ICON_SHOPON from './shopon.jpg';
 import ICON_LOLIMOYA from './lolimoya.jpg';
 export default {
+    ICON_DUMMY,
     ICON_AMANO,
     ICON_UTOI,
     ICON_KAGE,
